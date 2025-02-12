b998d48ce7beab5d9bf2165e2cb1c3cf24157036
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2025-02-11 20:47:25 -0800

    tablet screen size
    fixed main-layout grid for tablet screen size

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const resultsHtml = document.getElementById('results-section')
 
 resultsHtml.innerHTML = `
     <img src="../assets/images/illustration-empty.svg" alt="picture of money, calculator, and a board, illustrating an empty result" class="empty-result-icon">
-    <h2 class="empty-result-title">Results shown here</h2>
-    <p class="empty-result-subtitle">Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+    <h2 class="result-title">Results shown here</h2>
+    <p class="result-subtitle">Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
 `
